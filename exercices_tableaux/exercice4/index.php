05c82e470d744349566cd42de7e65ac41a77e4a6
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-11 16:23:35 +0100

    modif exo4

--- exercices_tableaux/exercice4/index.php
+++ exercices_tableaux/exercice4/index.php
@@ -15,7 +15,9 @@
     </p>
 
     <?php
-
+    $months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"];
+    $months[7] = "août";
+    var_dump($months);
     ?>
 
 </body>
