02d87694fe6e94dd09c11dcba762b737b2a358fc
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-11 19:13:21 +0100

    exo 7 completed

--- exercices_tableaux/exercice7/index.php
+++ exercices_tableaux/exercice7/index.php
@@ -11,11 +11,27 @@
     <h1># PHP - Les tableaux</h1>
 
     <p>## Exercice 7 <br>
-        Avec le tableau de l'exercice 5, ajouter la ligne correspondant au département de la ville de Reims.
+        Avec le tableau de l'exercice 5, ajouter la ligne correspondant au département de la ville de Reims. (51 Marne)
     </p>
 
     <?php
+    $hautsDeFrance = [
+        '02' => 'Aisne',
+        '59' => 'Nord',
+        '60' => 'Oise',
+        '62' => 'Pas-de-Calais',
+        '80' => 'Somme',
+    ];
 
+    // $hautsDeFrance = array (
+    //     '02' => 'Aisne',
+    //     '59' => 'Nord',
+    //     '60' => 'Oise',
+    //     '62' => 'Pas-de-Calais',
+    //     '80' => 'Somme');
+
+    $hautsDeFrance['51'] = 'Marne';
+    var_dump($hautsDeFrance);
     ?>
 
 </body>
