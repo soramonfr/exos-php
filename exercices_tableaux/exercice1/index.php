88e5d12286152412aef75c5b3e1492c4ba06a75d
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-11 15:05:33 +0100

    exo 1 completed

--- exercices_tableaux/exercice1/index.php
+++ exercices_tableaux/exercice1/index.php
@@ -11,23 +11,24 @@
     <h1># PHP - Les tableaux</h1>
 
     <p>## Exercice 1 <br>
-    Créer un tableau **months** et l'initialiser avec les valeurs suivantes :<br>
-- **janvier**<br>
-- **février**<br>
-- **mars**<br>
-- **avril**<br>
-- **mai**<br>
-- **juin**<br>
-- **juillet**<br>
-- **aout**<br>
-- **septembre**<br>
-- **octobre**<br>
-- **novembre**<br>
-- **décembre**
+        Créer un tableau **months** et l'initialiser avec les valeurs suivantes :<br>
+        - **janvier**<br>
+        - **février**<br>
+        - **mars**<br>
+        - **avril**<br>
+        - **mai**<br>
+        - **juin**<br>
+        - **juillet**<br>
+        - **aout**<br>
+        - **septembre**<br>
+        - **octobre**<br>
+        - **novembre**<br>
+        - **décembre**
     </p>
 
     <?php
-
+    $months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+    var_dump($months);
     ?>
 
 </body>
