4d3e478f8d1f8eb812962cb54f5b90696f2e488a
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-14 14:19:25 +0100

    consignes init

---
+++ exercices_param_url/exercice1/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Les paramètres d'URL</title>
+</head>
+
+<body>
+    <h1># PHP - Les paramètres d'URL</h1>
+
+    <p>## Exercice 1 <br>
+        Voici l'URL à étudier : <br>
+        index.php?lastname=Tutor&firstname=Janine<br>
+        Faire une page index.php.<br>
+        Sur cette page faire 2 boutons :<br>
+        * Le premier "bouton" doit contenir un href qui pointe vers l'URL. Il faut ensuite tester sur cette même page que tous les paramètres existent :<br>
+        - S'ils sont présents les afficher.<br>
+        - Dans le cas contraire ne rien afficher.<br>
+        * Le deuxieme "bouton" doit permettre de revenir à la page index.php.
+    </p>
+
+    <?php
+
+    ?>
+
+</body>
+
+</html>
---
+++ exercices_param_url/exercice3/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Les paramètres d'URL</title>
+</head>
+
+<body>
+    <h1># PHP - Les paramètres d'URL</h1>
+
+    <p>## Exercice 3 <br>
+        Voici l'URL à étudier :<br>
+
+        index.php?startDate=01/09/2019&endDate=17/03/2020<br>
+
+        Faire une page index.php.<br>
+        Sur cette page faire 2 boutons :<br>
+
+        Le premier "bouton" doit contenir un href qui pointe vers l'URL. Il faut ensuite tester sur cette même page que tous les paramètres existent :<br>
+        - S'ils sont présents, les afficher.<br>
+        - Dans le cas contraire ne rien afficher.<br>
+        Le deuxieme "bouton" doit permettre de revenir à la page index.php.
+    </p>
+
+    <?php
+
+    }
+    ?>
+
+</body>
+
+</html>
