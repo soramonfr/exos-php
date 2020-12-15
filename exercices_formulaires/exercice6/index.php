2e59561b4c7d3bd069a697f4b97a82c70445b9c1
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-15 11:02:45 +0100

    exo 6 init

--- exercices_formulaires/exercice6/index.php
+++ exercices_formulaires/exercice6/index.php
@@ -15,6 +15,19 @@
         Utiliser qu'une seule page.
     </p>
 
+    <form action="index.php" method="get">
+        <label for="title">Civilité</label>
+        <select name="title" id="title">
+            <option value="Mr">Mr</option>
+            <option value="Mme">Mme</option>
+        </select>
+        <label for="lastName">Nom</label>
+        <input type="text" name="lastName" id="lastName">
+        <label for="firstName">Prénom</label>
+        <input type="text" name="firstName" id="firstName">
+        <input type="submit" value="Valider">
+    </form>
+    
     <?php
 
     ?>
