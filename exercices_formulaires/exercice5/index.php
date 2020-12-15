319b0188b2f191e3d3467c90b683703d0ce8e4fd
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-15 16:15:25 +0100

    maj ex 7 & 8 - retrait pathinfo

--- exercices_formulaires/exercice5/index.php
+++ exercices_formulaires/exercice5/index.php
@@ -18,9 +18,12 @@
         <br>
         Ce formulaire doit rediriger vers la page **index.php**.<br>
         Vous avez le choix de la méthode.<br>
+        ## Exercice 7<br>
+        Au formulaire de l'exercice 5, ajouter un champ d'envoi de fichier. Afficher en plus de ce qui est demandé à l'exercice 6, le nom et l'extension du fichier.
     </p>
 
-    <form action="index.php" method="get">
+    <!-- Exercice 5 -->
+    <!-- <form action="index.php" method="get">
         <label for="title">Civilité</label>
         <select name="title" id="title">
             <option value="Mr">Mr</option>
@@ -31,12 +34,31 @@
         <label for="firstName">Prénom</label>
         <input type="text" name="firstName" id="firstName">
         <input type="submit" value="Valider">
-    </form>
+    </form> -->
 
+    <!-- Ex 7 & 8 -->
     <?php
-
+    var_dump($_FILES);
+    if (isset($_POST['lastName']) && isset($_POST['firstName']) && isset($_POST['title']) && isset($_FILES['pdfFile'])) {
+        echo $_POST['title'] . " " . $_POST['lastName'] . " " . $_POST['firstName'] . "<br>" . $_FILES['pdfFile']['name'];
+    } else {
     ?>
-
+        <form action="index.php" method="post" enctype="multipart/form-data">
+            <label for="title">Civilité</label>
+            <select name="title">
+                <option value="Mr">Mr</option>
+                <option value="Mme">Mme</option>
+            </select>
+            <label for="lastName">Nom</label>
+            <input type="text" name="lastName">
+            <label for="firstName">Prénom</label>
+            <input type="text" name="firstName">
+            <label for="pdfFile">Ajouter un fichier:</label>
+            <input type="file" name="pdfFile" accept="application/pdf">
+            <input type="submit" value="Valider">
+        </form>
+    <?php }
+    ?>
 
 </body>
 
