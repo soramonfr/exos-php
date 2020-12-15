1369124115cee7fab7d816daac6b1ddbf8b97b56
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-15 14:15:15 +0100

    ex6 maj

--- exercices_formulaires/exercice6/index.php
+++ exercices_formulaires/exercice6/index.php
@@ -15,23 +15,28 @@
         Utiliser qu'une seule page.
     </p>
 
-    <form action="index.php" method="get">
-        <label for="title">Civilité</label>
-        <select name="title" id="title">
-            <option value="Mr">Mr</option>
-            <option value="Mme">Mme</option>
-        </select>
-        <label for="lastName">Nom</label>
-        <input type="text" name="lastName" id="lastName">
-        <label for="firstName">Prénom</label>
-        <input type="text" name="firstName" id="firstName">
-        <input type="submit" value="Valider">
-    </form>
     
     <?php
-
+    if (isset($_GET['lastName']) && isset($_GET['firstName']) && isset($_GET['title'])) {
+        echo $_GET['title'] . " " . $_GET['lastName'] . " " . $_GET['firstName'];
+    } else {
+        ?>
+        <form action="index.php" method="get">
+            <label for="title">Civilité</label>
+            <select name="title" id="title">
+                <option value="Mr">Mr</option>
+                <option value="Mme">Mme</option>
+            </select>
+            <label for="lastName">Nom</label>
+            <input type="text" name="lastName" id="lastName">
+            <label for="firstName">Prénom</label>
+            <input type="text" name="firstName" id="firstName">
+            <input type="submit" value="Valider">
+        </form>
+    <?php }
     ?>
 
 </body>
 
 </html>
+
