d76754ad9eec265fffcc630aaae1f82ceed05e3d
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-17 14:46:19 +0100

    maj assets & input max

---
+++ tp_php/assets/script.js
@@ -0,0 +1,20 @@
+/*
+_________________________________________________________________________________________________________________________
+-------------------------------------------------------------------------------------------------------------------------
+Il faut créer au préalable un élément de type <img id="preview"> dans votre code html.
+Celui-ci vous permettra d'afficher l'aperçu de l'image.
+Vous allez pouvoir modifier les dimensions de l'aperçu via un css respectif : "uploadPreview.css" fourni dans le dossier.
+Il faut également que votre input soit de cette forme :
+<input type="file" name="fileToUpload" id="fileToUpload">
+-------------------------------------------------------------------------------------------------------------------------
+_________________________________________________________________________________________________________________________
+*/
+
+fileToUpload.addEventListener("change", function () {
+	let input = this;
+	let oFReader = new FileReader(); // on créé un nouvel objet FileReader
+	oFReader.readAsDataURL(this.files[0]);
+	oFReader.onload = function (oFREvent) {
+		imgPreview.setAttribute('src', oFREvent.target.result);
+	};
+})
--- tp_php/index.php
+++ tp_php/index.php
@@ -4,22 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
     <title>PHP - Upload d'images</title>
 </head>
 
 <body>
     <h1># PHP - Upload d'images</h1>
-
-    <form action="index.php" method="post" enctype="multipart/form-data">
-        <label for="imgFile">Veuillez choisir une image:</label>
-        <input type="file" name="imgFile" id='imgFile' accept="image/png, image/jpeg, image/gif, image/png">
-        <input type="submit" value="Valider">
-    </form>
+    <div class="form">
+        <form action="index.php" method="post" enctype="multipart/form-data">
+            <label for="imgFile">Veuillez choisir une image:</label>
+            <input type="hidden" name="MAX_FILE_SIZE" value="1048576">
+            <input type="file" name="imgFile" id='imgFile' accept="image/png, image/jpeg, image/gif, image/png">
+            <input type="submit" value="Valider">
+        </form>
+    </div>
 
     <?php
 
     ?>
 
+    <script src="assets/script.js"></script>
 </body>
 
 </html>
