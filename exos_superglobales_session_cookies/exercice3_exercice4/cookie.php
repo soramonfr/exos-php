2310792f06f6b636415a0edeb8ae805050998c29
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-22 11:19:14 +0100

    maj conditions

--- exos_superglobales_session_cookies/exercice3_exercice4/cookie.php
+++ exos_superglobales_session_cookies/exercice3_exercice4/cookie.php
@@ -2,6 +2,6 @@
 if (isset($_COOKIE["login"]) && (isset($_COOKIE["password"]))) {
     echo "Login: " . $_COOKIE["login"] . "<br>" . "Password: " . $_COOKIE["password"];
 } else {
-    echo "Les cookies n'ont pas été déterminés.";
+    echo "⛔ Les cookies n'ont pas été déterminés.";
 }
 ?>
