8fddb08d3b965c4f1362e402623fab6863217eec
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-22 11:18:19 +0100

    maj exo 3

---
+++ exos_superglobales_session_cookies/exercice3_exercice4/cookie.php
@@ -0,0 +1,7 @@
+<?php
+if (isset($_COOKIE["login"]) && (isset($_COOKIE["password"]))) {
+    echo "Login: " . $_COOKIE["login"] . "<br>" . "Password: " . $_COOKIE["password"];
+} else {
+    echo "Les cookies n'ont pas été déterminés.";
+}
+?>
---
+++ exos_superglobales_session_cookies/exercice3_exercice4/index.php
@@ -0,0 +1,42 @@
+<?php
+if (isset($_POST["submit"])) {
+    $login = $_POST["login"];
+    $password  = $_POST["password"];
+    setcookie("login", $login, time()+600);
+    setcookie("password", $password, time()+600);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Variables superglobales, sessions et cookies</title>
+</head>
+
+<body>
+    <h1># PHP - Variables superglobales, sessions et cookies</h1>
+
+    <p>## Exercice 3 <br>
+        Faire un formulaire qui permet de récupérer le login et le mot de passe de l'utilisateur. A la validation du formulaire, stocker les informations dans un cookie.
+    </p>
+    <p>## Exercice 4 <br>
+        Faire une page qui va récupérer les informations du cookie créé à l'exercice 3 et qui les affiches.
+    </p>
+    <form action="index.php" method="post">
+        <label for="login">Nom d'utilisateur</label>
+        <input type="text" name="login" id="login">
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password">
+        <input type="submit" name="submit" value="Valider">
+    </form>
+
+<?php
+
+?>
+
+</body>
+
+</html>
