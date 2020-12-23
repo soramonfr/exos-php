8917a2327cd983dd25944a9960a3db3e228fb022
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-23 11:50:37 +0100

    maj exo4

--- exos_dates/exercice1_to_exercice9/index.php
+++ exos_dates/exercice1_to_exercice9/index.php
@@ -11,37 +11,57 @@
 ## Exercice 1 <br>
 Afficher la date courante en respectant la forme **jj/mm/aaaa** *(ex : 16/05/2016)*<br>
 <br>
+
 <?php 
 $today = date("j/m/Y");
-echo $today;
+echo "=> ${today}";
 ?>
+
 <br>
 <br>
 ## Exercice 2<br>
 Afficher la date courante en respectant la forme **jj-mm-aa** *(ex : 16-05-16)*<br>
 <br>
+
 <?php 
 $today = date("j-m-y");
-echo $today;
+echo "=> ${today}";
 ?>
+
 <br>
 <br>
 ## Exercice 3<br>
 Afficher la date courante avec le jour de la semaine et le mois en toutes lettres *(ex : mardi 2 août 2016)*  <br>
-**Bonus** : Le faire en français.<br>
-<br>
+**Bonus** : Le faire en français.<br><br>
+
+
 <?php 
 setlocale(LC_ALL, "fr_FR.UTF8", "fr.UTF8", "fr_FR.UTF-8", "fr.UTF-8","fra");
 $today = strftime("%A %d %B %Y");
-echo $today;
+echo "=> ${today}";
 ?>
-<br>
-<br>
+
+<br><br>
 ## Exercice 4<br>
 Afficher le timestamp du jour.<br>  
-Afficher le timestamp du mardi 2 août 2016 à 15h00.<br>
+Afficher le timestamp du mardi 2 août 2016 à 15h00.<br><br>
 
-<br>
+<?php 
+date_default_timezone_set("Europe/Paris");
+setlocale(LC_ALL, "fr_FR.UTF8", "fr.UTF8", "fr_FR.UTF-8", "fr.UTF-8","fra");
+
+// Timestamp du jour
+$currentTimestamp = time();
+echo "=> Timestamp du jour: ${currentTimestamp}. <br>";
+
+
+// Mardi 2 août 2016 à 15h00
+$timestamp = mktime(15, 0, 0, 8, 2, 2016);
+echo "=> Timestamp du mardi 2 août 2016 à 15h00: ${timestamp}.";
+// Affichage du timestamp au format souhaité: echo date("d/m/Y h:i:sa", $timestamp);
+?>
+
+<br><br>
 ## Exercice 5<br>
 Afficher le nombre de jour qui sépare la date du jour avec le 16 mai 2016.<br>
 
