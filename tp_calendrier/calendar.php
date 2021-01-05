16ff1548636f71c295ff87def9fbea1063cbfdc6
sorayacruchet <soraya.cruchet@gmail.com>
2021-01-05 14:48:38 +0100

    génération de thead

--- tp_calendrier/calendar.php
+++ tp_calendrier/calendar.php
@@ -1,20 +1,15 @@
-<?php 
+<?php
 $days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
 $months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 $br = "<br>";
 
 if (!isset($_GET["months"])) {
     echo "🚧 Veuillez saisir un mois" . $br;
-} else {
-    echo "Le mois saisi est: " . $_GET["months"] . $br;
-}
+} 
 
 if (!isset($_GET["years"])) {
     echo "🚧 Veuillez saisir une année" . $br;
-} else {
-    echo "L'année saisie est: " . $_GET["years"] . $br;
-}
-
+} 
 ?>
 
 <!DOCTYPE html>
@@ -27,7 +22,28 @@ if (!isset($_GET["years"])) {
 </head>
 
 <body>
-<a href="index.php"><button type="submit">Retour au formulaire</button></a>
+    <?php
+    if (isset($_GET["months"]) && isset($_GET["years"])) {
+    ?>
+        <h1 class="text-center text-primary m-4"><?= $_GET["months"] . " " . $_GET["years"] ?></h1>
+    <?php
+    }
+    ?>
+
+    <table class="table table-bordered">
+        <thead class="title text-center text-white bg-dark">
+            <tr>
+                <?php
+                foreach ($days as $day) {
+                ?>
+                    <th style="width: 100px;"><?= $day ?></th>
+                <?php
+                }
+                ?>
+            </tr>
+        </thead>
+    </table>
+    <a href="index.php"><button type="submit">Retour au formulaire</button></a>
 
 
 </body>
